c84f71ec3346513eeb0d7c5c058746a8802338b1
andrelombardo <andre.lombardo25@gmail.com>
2022-12-10 19:36:06 +0100

    Aggiunta memorizzazione dati utente al database

--- src/pServer/ServerThread.java
+++ src/pServer/ServerThread.java
@@ -5,6 +5,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ServerThread extends Thread {
 	
@@ -27,23 +31,44 @@ public class ServerThread extends Thread {
 	
 	// Codice del server.
 	public void pServer() {
-		int clientNumber;
+		int numeroControllo; // Serve a capire cosa deve fare il server in base alla scelta del client.
+		String nomeUtente = ""; // Contiene il nome dell'utente.
+		String cognomeUtente = ""; // Contiene il cognome dell'utente.
+		String numeroUtente = ""; // Contiene il numero telefonico dell'utente.
 		try {
 			// Inizializzazione dei due stream di input e output.
 			inStream = new DataInputStream (externalClient.getInputStream());
 			outStream = new DataOutputStream(externalClient.getOutputStream());
 			
 			// E' un numero che manada il client: se 0 la finestra del login è chiusa se 1 ha mandato le credenziali.
-			clientNumber = inStream.readInt(); 
+			numeroControllo = inStream.readInt(); 
 			
-			if(clientNumber == 1) {
+			if(numeroControllo == 1) {
 				
-				System.out.println(inStream.readUTF());
-				System.out.println(inStream.readUTF());
-				System.out.println(inStream.readUTF());
+				// Riceve dal client nomeUtente, cognomeUtente e numeroUtente.
+				nomeUtente = inStream.readUTF();
+				cognomeUtente = inStream.readUTF();
+				numeroUtente = inStream.readUTF();
+	
+				try {
+					// Connessione al database.
+					Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Whatsapprs", "root", "");
+					// Dichiarazione.
+					Statement myStatement = myConnection.createStatement();
+					
+					// Aggiunta di dati al database.
+					String credenzialiUtente = "insert into utentiRegistrati (nomeUtente,cognomeUtente,numeroUtente)"
+							   + "value ('" + nomeUtente + "','" + cognomeUtente + "','" + numeroUtente + "')";
+					
+					// Aggiunta della stringa sql al database.
+				    myStatement.executeUpdate(credenzialiUtente);
+					
+				} catch (SQLException exception) {
+					exception.printStackTrace();
+				}
 			}
 			
-			if(clientNumber == 0) {
+			if(numeroControllo == 0) {
 				// Chiude i due stream di input e output.
 				inStream.close();
 				outStream.close();
